af8aeee6d3faa01167d8b758787ad5048a94e41f
Sidarth-Roy <73904891+Sidarth-Roy@users.noreply.github.com>
2021-10-07 23:25:09 +0530

    2nd

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>Namaste World!</h1>
+    <p>This is may be a paragraph.</p>
   </div>,
   document.getElementById("root")
 );
